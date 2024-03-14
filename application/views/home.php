9be284165554845b8d770217d7f8e1786d16c996
VimothSusara <vimothsusara@gmail.com>
2024-03-14 23:02:34 +0530

    created the hero section

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,28 @@
-<?php include ('templates/header.php')?>
+<?php include('templates/header.php') ?>
 
-<h1>Welcome Quiz</h1>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="grid text-start" style="--bs-gap: .25rem 1rem; margin-top:6.5rem;">
+                <div class="g-col-10 fw-semibold fs-1">Learn</div>
+                <div class="g-col-10 fw-semibold fs-1">new concepts</div>
+                <div class="g-col-10 fw-semibold fs-1 ">for each question</div>
+            </div>
+            <div class="d-flex justify-content-start mt-4">
+                <div class="p-1 border-2 border-start border-secondary"></div>
+                <div class="p-1 flex-grow-1 ms-2">We help you prepare for exam and quizzes</div>
+            </div>
+            <div class="d-flex justify-content-start mt-4">
+                <div class="p-2">
+                    <button type="button" class="btn btn-outline-secondary fs-4 ps-5 pe-5 fw-semibold" style="">Start</button>
+                </div>
+                <a href="#" class="text-decoration-none fs-4 pt-3 ps-5" style="color:rgb(13,67,94);">More</a>
+            </div>
+        </div>
+        <div class="col text-center">
+            <img src="<?= base_url('assets/images/heroPic.jpg'); ?>" class="img-fluid p-3">
+        </div>
+    </div>
+</div>
 
-<?php include ('templates/footer.php')?>
+<?php include('templates/footer.php') ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,16 +12,16 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="<?php echo base_url() ?>">Navbar</a>
+            <a class="navbar-brand" href="<?php echo base_url() ?>">QuizHub</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
+                    <li class="nav-item border">
                         <a class="nav-link" href="<?php echo base_url('index.php/home/register'); ?>">Register</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item border">
                         <a class="nav-link" href="<?php echo base_url('index.php/home/login'); ?>">Login</a>
                     </li>
                 </ul>
