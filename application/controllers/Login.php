ff4dbe530da758b4d79bdef188a0d8b1134b03f3
VimothSusara <vimothsusara@gmail.com>
2024-03-14 19:03:32 +0530

    solved the unable to login issue(was missing the sha1 in password when inserting data during login)

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -10,16 +10,15 @@ class Login extends CI_Controller
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'required');
 
-        if ($this->form_validation->run() === false) {
+        if ($this->form_validation->run() == false) {
             $this->load->view('login');
-        }
-        else{
+        } else {
             $this->load->model('Model_User');
             $result = $this->Model_User->loginUser();
             // print_r($result);
             // echo $result;
             if ($result != false) {
-                
+
                 $user_data = array(
                     'user_id' => $result->id,
                     'fname' => $result->fname,
@@ -31,17 +30,17 @@ class Login extends CI_Controller
                 // to manage user specific data you can use set_userdata() method
                 $this->session->set_userdata($user_data);
                 $this->session->set_flashdata('welcome', 'Welcome');
-            }
-            else {
+                redirect('Admin/index');
+            } else {
                 // echo $result;
                 $this->session->set_flashdata('errmsg', 'Wrong username or password!');
                 redirect('home/login');
             }
         }
-
     }
 
-    public function logoutUser() {
+    public function logoutUser()
+    {
         $this->session->unset_userdata('user_id');
         $this->session->unset_userdata('fname');
         $this->session->unset_userdata('lname');
@@ -49,5 +48,4 @@ class Login extends CI_Controller
         $this->session->unset_userdata('logedIn');
         redirect('home/login');
     }
-
 }
--- application/models/Model_User.php
+++ application/models/Model_User.php
@@ -9,10 +9,10 @@ class Model_User extends CI_Model
 
         // echo "Test";
         $data = array(
-            'fname' => $this->input->post('fname',TRUE),
-            'lname' => $this->input->post('lname',TRUE),
-            'email' => $this->input->post('email',TRUE),
-            'password' => sha1($this->input->post('password',TRUE)),
+            'fname' => $this->input->post('fname', TRUE),
+            'lname' => $this->input->post('lname', TRUE),
+            'email' => $this->input->post('email', TRUE),
+            'password' => sha1($this->input->post('password', TRUE)),
         );
 
         // print_r($data);
@@ -22,23 +22,21 @@ class Model_User extends CI_Model
 
     }
 
-    function loginUser() {
+    function loginUser()
+    {
 
         $email = $this->input->post('email');
-        $password = $this->input->post('password');
+        $password = sha1($this->input->post('password'));
 
         $this->db->where('email', $email);
         $this->db->where('password', $password);
 
         $respond = $this->db->get('users');
-        if ($respond->num_rows() > 0){
+        if ($respond->num_rows() == 1) {
             // print_r($respond->row(0));
             return $respond->row(0);
-        }
-        else{
+        } else {
             return false;
         }
-
     }
-
 }
